388c633c657a3ffac36167c8071a3ef4ee1bcc9a
songyuanming <songyuanming@allydata.cn>
2016-11-17 19:13:22 +0800

    视图控制器基类

---
+++ Weibo/Classes/Tools/Extensions/UILabel+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UILabel+Extension.swift
+//  Weibo
+//
+//  Created by 宋元明 on 16/11/17.
+//  Copyright © 2016年 宋元明. All rights reserved.
+//
+
+import Foundation
---
+++ Weibo/Classes/View(视图和控制器)/Main/WBVisitorView.swift
@@ -0,0 +1,21 @@
+//
+//  WBVisitorView.swift
+//  Weibo
+//
+//  Created by 宋元明 on 16/11/17.
+//  Copyright © 2016年 宋元明. All rights reserved.
+//
+
+import UIKit
+
+class WBVisitorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
