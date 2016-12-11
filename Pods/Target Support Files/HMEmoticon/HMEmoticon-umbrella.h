3d867a7d54c26f5c841d7e470b9feb61956c13fc
songyuanming <songyuanming@allydata.cn>
2016-12-11 19:53:39 +0800

    补充

---
+++ Pods/Target Support Files/HMEmoticon/HMEmoticon-umbrella.h
@@ -0,0 +1,19 @@
+#import <UIKit/UIKit.h>
+
+#import "HMEmoticon.h"
+#import "HMEmoticonAttachment.h"
+#import "HMEmoticonButton.h"
+#import "HMEmoticonCell.h"
+#import "HMEmoticonInputView.h"
+#import "HMEmoticonManager.h"
+#import "HMEmoticonPackage.h"
+#import "HMEmoticonTextView.h"
+#import "HMEmoticonTipView.h"
+#import "HMEmoticonToolbar.h"
+#import "NSBundle+HMEmoticon.h"
+#import "NSString+HMEmoji.h"
+#import "UIImage+HMEmoticon.h"
+
+FOUNDATION_EXPORT double HMEmoticonVersionNumber;
+FOUNDATION_EXPORT const unsigned char HMEmoticonVersionString[];
+
